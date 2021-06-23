ab9a803e0918d5345bd84e82fb75f134da1fd0df
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-06-23 17:18:20 -0500

    Added problem description

--- (02) Transcribing DNA into RNA.py
+++ (02) Transcribing DNA into RNA.py
@@ -1,3 +1,8 @@
+# Given: A DNA string having length at most 1000 nt.
+
+# Return: The transcribed RNA string of the DNA string.
+
+
 DNAsequence = open('rosalind_rna.txt', 'r')
 
 # Substitute uracil for thymine to transcribe sequence
