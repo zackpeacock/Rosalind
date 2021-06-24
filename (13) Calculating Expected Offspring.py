d6579ac95447643d97e0a34ce1bef4c6a87d81fb
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-06-24 12:37:47 -0500

    Added problem description

--- (13) Calculating Expected Offspring.py
+++ (13) Calculating Expected Offspring.py
@@ -1,3 +1,16 @@
+# Given: Six nonnegative integers, each of which does not exceed 20,000. The integers correspond to the number of couples in a population possessing
+# each genotype pairing for a given factor. In order, the six given integers represent the number of couples having the following genotypes:
+
+# 1. AA-AA
+# 2. AA-Aa
+# 3. AA-aa
+# 4. Aa-Aa
+# 5. Aa-aa
+# 6. aa-aa
+
+#Return: The expected number of offspring displaying the dominant phenotype in the next generation, under the assumption that every couple has exactly two offspring.
+
+
 # Store given values in list
 file = open('rosalind_iev.txt', 'r')
 givenIntegers = file.read().strip().split(' ')
