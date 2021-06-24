8d65e1dd4c8960dcd2d5cefe8ae2c09bce8e21bb
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-06-24 12:30:10 -0500

    Added problem description

--- (07) Mendel's First Law.py
+++ (07) Mendel's First Law.py
@@ -1,3 +1,10 @@
+# Given: Three positive integers k, m, and n, representing a population containing k + m + n organisms: k individuals are homozygous dominant for a factor, 
+# m are heterozygous, and n are homozygous recessive.
+
+# Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele 
+# (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
+
+
 integers = open('rosalind_iprb.txt', 'r')
 for string in integers:
     integerList = string.strip().split(' ')
