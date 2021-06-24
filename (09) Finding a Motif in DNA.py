391052d42ab67c173ba8e5bd9836e80a9608586f
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-06-24 12:31:53 -0500

    Added problem description

--- (09) Finding a Motif in DNA.py
+++ (09) Finding a Motif in DNA.py
@@ -1,3 +1,8 @@
+# Given: Two DNA strings s and t (each of length at most 1 kbp).
+
+# Return: All locations of t as a substring of s.
+
+
 file = open('rosalind_subs.txt', 'r')
 
 # Assign sequence and subsequence to variables
