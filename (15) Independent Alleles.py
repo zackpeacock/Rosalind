81871e0f09d07ffb6c6d7a23ea36065e3acff9f1
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-06-28 17:41:50 -0400

    Added problem description

--- (15) Independent Alleles.py
+++ (15) Independent Alleles.py
@@ -1,3 +1,10 @@
+# Given: Two positive integers k (k ≤ 7) and N (N ≤ 2^k). In this problem, we begin with Tom, who in the 0th generation has genotype Aa Bb. 
+# Tom has two children in the 1st generation, each of whom has two children, and so on. Each organism always mates with an organism having genotype Aa Bb.
+
+# Return: The probability that at least N Aa Bb organisms will belong to the k-th generation of Tom's family tree (don't count the Aa Bb mates at each level). 
+# Assume that Mendel's second law holds for the factors.
+
+
 # Assign given values for k and N to variables
 import math
 file = open('rosalind_lia.txt', 'r')
