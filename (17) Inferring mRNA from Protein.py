78f2bb19b0e5a3a0fd3d8eb8b904d2737c4b7bea
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-06-29 15:11:01 -0400

    Upload solution

---
+++ (17) Inferring mRNA from Protein.py
@@ -0,0 +1,41 @@
+file = open('rosalind_mrna.txt', 'r')
+protein = file.read().strip()
+
+# Copy/paste preexisting codon: amino acid dictionary
+codons = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G',
+}
+
+# Invert codon dictionary such that keys are amino acids and values are codons
+inverted_codons = {}
+for i in codons:
+    for j in codons:
+        if codons[i] == codons[j]:
+            inverted_codons.setdefault(codons[i], [i]).append(j)
+
+# Remove duplicate codons
+for i in inverted_codons:
+    inverted_codons[i] = list(set(inverted_codons[i]))
+
+# Multiply number of codons for each amino acid
+possible_strings = 1
+for aa in protein:
+    possible_strings = possible_strings * (len(inverted_codons[aa]))
+
+# Finally, multiply by 3 to account for 3 stop codons
+print((possible_strings * 3) % 1000000)
