6bbdb55b2d6b0f1b0a15bf978794ee833770345f
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-06-28 23:25:16 -0400

    Added problem description

--- (16) Finding a Protein Motif.py
+++ (16) Finding a Protein Motif.py
@@ -1,3 +1,9 @@
+# Given: At most 15 UniProt Protein Database access IDs.
+
+# Return: For each protein possessing the N-glycosylation motif ( N{P}[ST]{P} ), output its given access ID followed by a list of locations in the protein string
+# where the motif can be found.
+
+
 import requests
 
 # Compile given protein IDs into list
