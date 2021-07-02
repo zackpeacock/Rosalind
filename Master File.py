f615e849b1fcf12d86a8629855fb0dfabc569592
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-07-02 16:14:32 -0400

    Update file

--- Master File.py
+++ Master File.py
@@ -655,3 +655,30 @@ edited_protein_candidates = list(set(protein_candidates))
 # Print candidates
 for candidate in edited_protein_candidates:
     print(candidate)
+
+
+# *** Enumerating Gene Orders (PERM) ***
+
+from itertools import permutations
+import math
+
+# Assign given integer to variable n
+file = open('rosalind_perm.txt', 'r')
+given_integer = file.read().split()
+n = int(given_integer[0])
+
+# Create list of permutations of n
+perms = list(permutations(range(1, n + 1)))
+
+# Format list of permutations
+formatted_perms = []
+for lst in perms:
+    sublist = []
+    for i in lst:
+        sublist.append(str(i))
+    formatted_perms.append(sublist)
+
+# Print number of permutations and all possible permutations in proper format
+print(math.factorial(n))
+for lst in formatted_perms:
+    print(' '.join(lst))
