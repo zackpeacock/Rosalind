41e1e19ea8206c5097e75370c1f382eca24eaa1f
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-07-02 16:14:04 -0400

    Upload solution

---
+++ (19) Enumerating Gene Orders.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+import math
+
+# Assign given integer to variable n
+file = open('rosalind_perm.txt', 'r')
+given_integer = file.read().split()
+n = int(given_integer[0])
+
+# Create list of permutations of n
+perms = list(permutations(range(1, n + 1)))
+
+# Format list of permutations
+formatted_perms = []
+for lst in perms:
+    sublist = []
+    for i in lst:
+        sublist.append(str(i))
+    formatted_perms.append(sublist)
+
+# Print number of permutations and all possible permutations in proper format
+print(math.factorial(n))
+for lst in formatted_perms:
+    print(' '.join(lst))
