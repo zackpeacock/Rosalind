07571aeeec64607f3d79e8656c5eafffb471d2ac
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-07-12 18:55:06 -0400

    Added problem description

--- (23) Enumerating k-mers Lexicographically.py
+++ (23) Enumerating k-mers Lexicographically.py
@@ -1,3 +1,8 @@
+# Given: A collection of at most 10 symbols defining an ordered alphabet, and a positive integer n (n ≤ 10).
+
+# Return: All strings of length n that can be formed from the alphabet, ordered lexicographically (use the standard order of symbols in the English alphabet).
+
+
 import itertools
 
 # Create and format alphabet list from .txt file, assign string length to variable
