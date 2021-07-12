3432daafbb25aa5e83cfbd5e4f7bc9187e3c7249
zackpeacock <80722224+zackpeacock@users.noreply.github.com>
2021-07-12 16:54:33 -0400

    Added problem description

--- (21) Locating Restriction Sites.py
+++ (21) Locating Restriction Sites.py
@@ -1,3 +1,7 @@
+# Given: A DNA string of length at most 1 kbp in FASTA format.
+
+# Return: The position and length of every reverse palindrome in the string having length between 4 and 12. You may return these pairs in any order.
+
 # Function to isolate DNA sequence from FASTA file
 def IsolateSequence(filename):
     file = open(filename, 'r')
